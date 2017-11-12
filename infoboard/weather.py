b10cb1194acb3eb1a7e2b765fcf7f37b18a9ee76
Andy Stiller <andy@andyhat.co.uk>
2017-11-12 19:23:18 +0000

    Add new modules for display formatting.

---
+++ infoboard/weather.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+Weather module takes the weather information in a weather provider
+and disaplays the conditions for the current hour.
+"""
+from providers import metcheck_weather
+

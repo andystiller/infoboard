428acc9909ee65dae57d28ed2455be50a2718f3d
Andy Stiller <andy@andyhat.co.uk>
2017-11-25 21:05:59 +0000

    Update the yahoo news provider

--- infoboard/providers/yahoo_news.py
+++ infoboard/providers/yahoo_news.py
@@ -8,27 +8,29 @@ It uses thethe free rss feed
 import feedparser
 from unidecode import unidecode
 
-class Yahoo_news(object):  # added object base class for python2 compatibility.
+class YahooNews(object):  # added object base class for python2 compatibility.
     """
     The Yahoo_news class handles getting and processing the news feeed
     """
     __YAHOO_NEWS_URL = 'https://uk.news.yahoo.com/rss'
 
     def __init__(self):
-        feed = feedparser.parse(self.__YAHOO_NEWS_URL)
-        for key in feed["entries"]:
+        self._feed = None
+        self.update()
+
+    def update(self):
+        self._feed = feedparser.parse(self.__YAHOO_NEWS_URL)
+        for key in self._feed["entries"]:
             print(unidecode(key["title"]))
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
+    def news(self):
+        return self._feed
 
 def main():
     """
     Entry point for testing if the file is run on it's own.
     """
-    news_feed = Yahoo_news()
+    news_feed = YahooNews()
 
 if __name__ == "__main__":
     main()

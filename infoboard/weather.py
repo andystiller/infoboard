c200ee8cda74888692605bab4a2c7d1f8e09fe5b
Andy Stiller <andy@andyhat.co.uk>
2017-12-16 19:15:45 +0000

    Add test code to weather forecast handler

--- infoboard/weather.py
+++ infoboard/weather.py
@@ -3,5 +3,15 @@
 Weather module takes the weather information in a weather provider
 and disaplays the conditions for the current hour.
 """
-from providers import metcheck_weather
+from providers.metcheck_weather import MetcheckWeather
 
+def main():
+    """
+    Entry point for testing if the file is run on it's own.
+    """
+    weather_forecast = MetcheckWeather()
+    print(weather_forecast.current_weather)
+    print(weather_forecast.next_hour)
+
+if __name__ == "__main__":
+    main()

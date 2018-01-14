21d753e66fb1dee2890e9d519a329d0d291f0734
Andy Stiller <andy@andyhat.co.uk>
2018-01-14 21:06:47 +0000

    Add a limited itterator to the YahooNews class.

--- infoboard/providers/yahoo_news.py
+++ infoboard/providers/yahoo_news.py
@@ -14,23 +14,47 @@ class YahooNews(object):  # added object base class for python2 compatibility.
     """
     __YAHOO_NEWS_URL = 'https://uk.news.yahoo.com/rss'
 
-    def __init__(self):
+    def __init__(self, limit = None):
         self._feed = None
+        self._iterator = None
+        self._index = 1
         self.update()
+        if limit:
+            self._limit = limit
+        else:
+            self._limit = max(self._feed)
 
     def update(self):
         self._feed = feedparser.parse(self.__YAHOO_NEWS_URL)
-        for key in self._feed["entries"]:
-            print(unidecode(key["title"]))
+        self._iterator = iter(self._feed['entries'])
 
     def news(self):
         return self._feed
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        story = None
+
+        if self._index <= self._limit:
+            story = self._iterator.__next__()
+            self._index += 1
+        else:
+            raise StopIteration
+        return story
 
 def main():
     """
     Entry point for testing if the file is run on it's own.
     """
-    news_feed = YahooNews()
+    news_feed = YahooNews(3)
+    feed = news_feed.news()
+    # for key in feed["entries"]:
+    #         print(unidecode(key["title"]))
+
+    for news_item in news_feed:
+        print(news_item['title'])
 
 if __name__ == "__main__":
     main()

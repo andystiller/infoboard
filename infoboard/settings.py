6e36cafd3bba14c7769517443a9c9fbba06e5023
Andy Stiller <andy@andyhat.co.uk>
2018-01-18 20:13:14 +0000

    Correct the settings handling.

--- infoboard/settings.py
+++ infoboard/settings.py
@@ -22,8 +22,8 @@ def get_settings(section, key):
         with open(SETTINGS_FILE, 'r') as configfile:
             config = json.load(configfile)
             setting = config[section][key]
-
-    if os.path.isfile(DEFAULT_SETTINGS):
+    
+    elif os.path.isfile(DEFAULT_SETTINGS):
         print(DEFAULT_SETTINGS)
         with open(DEFAULT_SETTINGS, 'r') as configfile:
             config = json.load(configfile)
